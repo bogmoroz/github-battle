5fa4394ce153862a8b8200ecd474aa30406a4be0
Bogdan Moroz <boggy.frost@gmail.com>
2019-08-18 19:59:52 +0300

    Battle component created

---
+++ app/components/Battle.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Instructions() {
+  return <div>INSTRUCTIONS</div>;
+}
+
+export default class Battle extends React.Component {
+  render() {
+    return (
+      <>
+        <Instructions />
+      </>
+    );
+  }
+}
