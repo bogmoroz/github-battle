2cab1576eac4f4d488c378dd0bb994a007ca90f7
Bogdan Moroz <boggy.frost@gmail.com>
2019-08-18 18:59:26 +0300

    Caching loaded repos

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -33,7 +33,7 @@ export default class Popular extends React.Component {
 
     this.state = {
       selectedLanguage: 'All',
-      repos: null,
+      repos: {},
       error: null
     };
 
@@ -48,27 +48,31 @@ export default class Popular extends React.Component {
   updateLanguage(selectedLanguage) {
     this.setState({
       selectedLanguage,
-      error: null,
-      repos: null
+      error: null
     });
 
-    fetchPopularRepos(selectedLanguage)
-      .then(repos =>
-        this.setState({
-          repos,
-          error: null
-        })
-      )
-      .catch(() => {
-        console.warn('Error fetching repos: ', error);
-        this.setState({
-          error: 'There was an error fetching the repositories'
+    if (!this.state.repos[selectedLanguage]) {
+      fetchPopularRepos(selectedLanguage)
+        .then(data =>
+          this.setState(({ repos }) => ({
+            repos: {
+              ...repos,
+              [selectedLanguage]: data
+            }
+          }))
+        )
+        .catch(() => {
+          console.warn('Error fetching repos: ', error);
+          this.setState({
+            error: 'There was an error fetching the repositories'
+          });
         });
-      });
+    }
   }
 
   isLoading() {
-    return this.state.repos === null && this.state.error === null;
+    const { selectedLanguage, repos, error } = this.state;
+    return !repos[selectedLanguage] && error === null;
   }
 
   render() {
