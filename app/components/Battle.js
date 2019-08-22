dde3102232f20ebe3400095c223f654c40bd99f2
Bogdan Moroz <boggy.frost@gmail.com>
2019-08-22 21:43:49 +0300

    Displaying results from Battle

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -6,6 +6,7 @@ import {
   FaTrophy
 } from 'react-icons/fa';
 import PropTypes from 'prop-types';
+import Results from './Results';
 
 function Instructions() {
   return (
@@ -125,7 +126,8 @@ export default class Battle extends React.Component {
     super(props);
     this.state = {
       playerOne: null,
-      playerTwo: null
+      playerTwo: null,
+      battle: false
     };
 
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -145,7 +147,12 @@ export default class Battle extends React.Component {
   }
 
   render() {
-    const { playerOne, playerTwo } = this.state;
+    const { playerOne, playerTwo, battle } = this.state;
+
+    if (battle === true) {
+      return <Results playerOne={playerOne} playerTwo={playerTwo} />;
+    }
+
     return (
       <>
         <Instructions />
@@ -177,6 +184,15 @@ export default class Battle extends React.Component {
               />
             )}
           </div>
+
+          {playerOne && playerTwo && (
+            <button
+              className='btn dark-btn btn-space'
+              onClick={() => this.setState({ battle: true })}
+            >
+              Battle
+            </button>
+          )}
         </div>
       </>
     );
