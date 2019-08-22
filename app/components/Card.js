e5d7c46031cee154b293690a08a073a8d1368e1f
Bogdan Moroz <boggy.frost@gmail.com>
2019-08-22 23:55:39 +0300

    Bug in Card fixed

--- app/components/Card.js
+++ app/components/Card.js
@@ -12,11 +12,7 @@ export default function Card({
   return (
     <div className='card bg-light'>
       <h4 className='header-lg center-text'>{header}</h4>
-      <img
-        className='avatar'
-        src={avatar}
-        alt={`Avatar for ${winner.profile.login}`}
-      />
+      <img className='avatar' src={avatar} alt={`Avatar for ${name}`} />
       {subheader && <h4 className='center-text'>{subheader}</h4>}
       <h2 className='center-text'>
         <a className='link' href={href}>
