425728c02514f7e9ab6eda3a900deb311779e311
Bogdan Moroz <boggy.frost@gmail.com>
2019-08-23 22:22:29 +0300

    Animating "Loading..." text

--- app/components/Loading.js
+++ app/components/Loading.js
@@ -9,6 +9,14 @@ export default class Loading extends React.Component {
     };
   }
 
+  componentDidMount() {
+    window.setInterval(() => {
+      this.state.content === 'Loading' + '...'
+        ? this.setState({ content: 'Loading' })
+        : this.setState(({ content }) => ({ content: content + '.' }));
+    }, 300);
+  }
+
   render() {
     return <p>{this.state.content}</p>;
   }
